a813942ffcecc86f4c52d8f8fb2c2427af28ab4c
Jmix-Dev <josephcham@gmail.com>
2023-03-27 15:48:45 +0200

    Initial commit

---
+++ src/components/reviewForm/ReviewForm.js
@@ -0,0 +1,11 @@
+import { Form, Button } from "react-bootstrap";
+
+const ReviewForm = () => {
+  return (
+    <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1"></Form.Group>
+    </Form>
+  );
+};
+
+export default ReviewForm;
